561669bff1792b2a70ba145957d965fa7ea8a52e
Jason Thomas <jmthomas@ball.com>
2014-07-31 14:10:10 -0600

    Add qtdeclarative to build

---
+++ lib/qtdeclarative/qtdeclarative.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+module QtDeclarative
+    module Internal
+        def self.init_all_classes
+            getClassList.each do |c|
+                classname = Qt::Internal::normalize_classname(c)
+                id = Qt::Internal::findClass(c);
+                Qt::Internal::insert_pclassid(classname, id)
+                Qt::Internal::cpp_names[classname] = c
+                klass = Qt::Internal::isQObject(c) ? Qt::Internal::create_qobject_class(classname, Qt) \
+                                                    : Qt::Internal::create_qt_class(classname, Qt)
+                Qt::Internal::classes[classname] = klass unless klass.nil?
+            end
+        end
+    end
+end
