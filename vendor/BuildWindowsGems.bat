572c1f60c7b9e1443d9e586a160fffdd6b141f95
Thomas, Jason <jmthomas@ball.com>
2019-11-14 15:01:11 -0700

    Update BuildQt4Win and remove from BuildWindowsGems

--- vendor/BuildWindowsGems.bat
+++ vendor/BuildWindowsGems.bat
@@ -7,7 +7,7 @@
 ::   b. Cmake
 ::   c. The 64-bit versions of msys2 for Ruby 2.6
 ::   d. Qt built with msys2 for 64-bit Ruby 2.6
-::      See BuildQt4Win64Ruby24.bat
+::      See BuildQt4Win64.bat
 ::   e. Ruby version 2.4, 2.5, 2.6 64-bit version
 ::   f. Update all the paths below to your installations
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -28,29 +28,13 @@ set RUBY24_64_PATH=C:\Ruby24-x64
 set RUBY25_64_PATH=C:\Ruby25-x64
 set RUBY26_64_PATH=C:\Ruby26-x64
 set MINGW_PATH=!RUBY26_64_PATH!\msys64\mingw64
-set PERL_PATH=C:\Strawberry\perl\bin
+set QTBINDINGS_QT_PATH=!QT_PATH!
+set PATH=!CMAKE_PATH!;!MINGW_PATH!\bin;!QT_PATH!\bin;!RUBY26_64_PATH!\bin
 
-cd !QT_PATH!
-copy "!MINGW_PATH!\bin\libgcc_s_seh-1.dll" bin
-copy "!MINGW_PATH!\bin\libstdc++-6.dll" bin
-copy "!MINGW_PATH!\bin\libwinpthread-1.dll" bin
-set INCLUDE=!MINGW_PATH!\x86_64-w64-mingw32\include;
-set LIB=!MINGW_PATH!\x86_64-w64-mingw32\lib;
-set PATH=!MINGW_PATH!\bin;!QT_PATH!;!QT_PATH!\bin;!PERL_PATH!
-echo !PATH!
-call configure -opensource -confirm-license -release -platform win32-g++-4.6 -opengl desktop -webkit -openssl -qt-style-windowsxp -qt-style-windowsvista -I C:\OpenSSL-Win64\include -L C:\OpenSSL-Win64 -nomake examples -nomake demos
-call make clean
-jom
-
-:: Go back to where we started
-cd "%~dp0"
 cd ..
 mkdir release
 
 :: Cleanup
-
-set QTBINDINGS_QT_PATH=!QT_PATH!
-set PATH=!CMAKE_PATH!;!MINGW_PATH!\bin;!QT_PATH!\bin;!RUBY24_64_PATH!\bin
 echo.
 echo Cleanup
 call ruby -e "puts RUBY_VERSION"
@@ -62,6 +46,7 @@ echo.
 echo Building 64-bit
 echo.
 
+set PATH=!RUBY24_64_PATH!\bin;!PATH!
 echo.
 echo Ruby
 call ruby -e "puts RUBY_VERSION"
