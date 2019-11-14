f2f3bf64c894ecf737819d5ae2cb7ddb7278c56c
Thomas, Jason <jmthomas@ball.com>
2019-11-14 11:57:11 -0700

    Change Win build to 64 only

--- vendor/BuildWindowsGems.bat
+++ vendor/BuildWindowsGems.bat
@@ -1,18 +1,18 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Builds Windows 32-bit and 64-bit gems for Windows
+:: Builds Windows 64-bit gems for Windows
 :: Usage: BuildWindowsGems VERSION
 ::
 :: Make sure you have installed the following before running:
 ::   a. Windows SDK for Windows version you are on
 ::   b. Cmake
-::   c. The 32-bit and 64-bit versions of msys2 for Ruby 2.4
-::   d. Qt built with msys for both 32-bit and 64-bit Ruby 2.4
-::      See BuildQt4Win32Ruby24.bat and BuildQt4Win64Ruby24.bat
-::   e. Ruby version 2.4 both 32-bit and 64-bit version
+::   c. The 64-bit versions of msys2 for Ruby 2.6
+::   d. Qt built with msys2 for 64-bit Ruby 2.6
+::      See BuildQt4Win64Ruby24.bat
+::   e. Ruby version 2.4, 2.5, 2.6 64-bit version
 ::   f. Update all the paths below to your installations
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-@echo off
+@echo on
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 IF [%1]==[] (
@@ -21,70 +21,36 @@ IF [%1]==[] (
 ) else (
   set COSMOS_INSTALL=%~1
 )
-set MSYS64_32_PATH=C:\msys64
-set MSYS64_64_PATH=C:\msys64
-set QT_32_RUBY24_PATH=C:\Qt\4.8.6-Ruby24
-set Qt_64_RUBY24_PATH=C:\Qt\4.8.6-64-Ruby24
-set RUBY24_32_PATH=C:\Ruby242
-set RUBY24_64_PATH=C:\Ruby242-x64
-set RUBY25_32_PATH=C:\Ruby251
-set RUBY25_64_PATH=C:\Ruby251-x64
 
-:: Down to the main directory
+set CMAKE_PATH="C:\Program Files\CMake\bin"
+set QT_PATH=C:\Qt\4.8.6
+set RUBY24_64_PATH=C:\Ruby24-x64
+set RUBY25_64_PATH=C:\Ruby25-x64
+set RUBY26_64_PATH=C:\Ruby26-x64
+set MINGW_PATH=!RUBY26_64_PATH!\msys64\mingw64
+set PERL_PATH=C:\Strawberry\perl\bin
+
+cd !QT_PATH!
+copy "!MINGW_PATH!\bin\libgcc_s_seh-1.dll" bin
+copy "!MINGW_PATH!\bin\libstdc++-6.dll" bin
+copy "!MINGW_PATH!\bin\libwinpthread-1.dll" bin
+set INCLUDE=!MINGW_PATH!\x86_64-w64-mingw32\include;
+set LIB=!MINGW_PATH!\x86_64-w64-mingw32\lib;
+set PATH=!MINGW_PATH!\bin;!QT_PATH!;!QT_PATH!\bin;!PERL_PATH!
+echo !PATH!
+call configure -opensource -confirm-license -release -platform win32-g++-4.6 -opengl desktop -webkit -openssl -qt-style-windowsxp -qt-style-windowsvista -I C:\OpenSSL-Win64\include -L C:\OpenSSL-Win64 -nomake examples -nomake demos
+call make clean
+jom
+
+:: Go back to where we started
+cd "%~dp0"
 cd ..
 mkdir release
 
-:: 32-bit version
-
 :: Cleanup
 
-set QTBINDINGS_QT_PATH=!QT_32_RUBY24_PATH!
-set PATH=!MSYS64_32_PATH!\mingw32\bin;!QT_32_RUBY24_PATH!\bin;!PATH!
-set PATH=!RUBY24_32_PATH!\bin;!PATH!
-echo.
-echo Cleanup
-call ruby -e "puts RUBY_VERSION"
-echo.
-call rake distclean
-
-:: Build 32-bit
-echo.
-echo Building 32-bit
-echo.
-
-echo.
-echo Ruby
-call ruby -e "puts RUBY_VERSION"
-echo.
-call rake build
-
-set PATH=!RUBY25_32_PATH!\bin;!PATH!
-echo.
-echo Ruby
-call ruby -e "puts RUBY_VERSION"
-echo.
-call rake build
-
-echo.
-echo Build 32-bit qtbindings gem
-echo.
-call rake VERSION=%1 gemnative
-
-echo.
-echo Build 32-bit qtbindings-qt gem
-echo.
-call rake VERSION=%1 gemqt
-
-move *.gem release
-move release\*.gemspec .
-
-:: 64-bit version
-
-:: Cleanup
-
-set QTBINDINGS_QT_PATH=!QT_64_RUBY24_PATH!
-set PATH=!MSYS64_64_PATH!\mingw64\bin;!QT_64_RUBY24_PATH!\bin;!PATH!
-set PATH=!RUBY24_64_PATH!\bin;!PATH!
+set QTBINDINGS_QT_PATH=!QT_PATH!
+set PATH=!CMAKE_PATH!;!MINGW_PATH!\bin;!QT_PATH!\bin;!RUBY24_64_PATH!\bin
 echo.
 echo Cleanup
 call ruby -e "puts RUBY_VERSION"
@@ -103,6 +69,13 @@ echo.
 call rake build
 
 set PATH=!RUBY25_64_PATH!\bin;!PATH!
+echo.
+echo Ruby
+call ruby -e "puts RUBY_VERSION"
+echo.
+call rake build
+
+set PATH=!RUBY26_64_PATH!\bin;!PATH!
 echo.
 echo Ruby
 call ruby -e "puts RUBY_VERSION"
